8a3299261ccbccf1e7a584a00c539278b470d0fa
Holger Mueller <9jholger@gmail.com>
2018-06-12 18:30:14 -0500

    got server working

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+//const db = require('./models');
+
+//create port
+const app = express();
+const PORT = process.env.PORT || 8080
+
+//import public folder
+app.use(express.static('public'));
+
+//bodyparser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//handlbars
+app.engine('handdlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//start server
+app.listen(PORT, function(){
+    console.log('server listening on localhost: ' + PORT);
+})
