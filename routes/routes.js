64e78309f32f5d966f9c4310bc07b42043e3b32a
Holger Mueller <9jholger@gmail.com>
2018-06-18 22:24:56 -0500

    test scrape works

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const db = require('../models');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
     //GET route for index
     app.get('/', function (req, res) {
@@ -12,10 +12,34 @@ module.exports = function(app) {
         });
     });
 
+    //scrapers
+    const axios = require('axios');
+    const cheerio = require('cheerio');
+
+    //test scrape
+    app.get("/scrape", function (req, res) {
+        axios.get("http://www.wired.com/category/science/").then(function (response) {
+            let $ = cheerio.load(response.data);
+            $(".card-component__description").each(function (i, element) {
+                let result = {};
+                result.headline = $(this).text();
+                result.summary = $(this).text();
+                result.link = $(this).children('a').attr('href');
+                console.log(result);
+
+                db.Article.create(result)
+                    .then(function (dbArticle) {
+                        console.log(dbArticle);
+                    }).catch(function (err) {
+                        return res.json(err);
+                    });
+            });
+            res.send("Scrape complete");
+        });
+    });
+
     //route to scrape articles
-//scrapers
-const axios = require('axios');
-const cheerio = require('cheerio');
+
 
     //route to get one article
 
@@ -32,5 +56,5 @@ const cheerio = require('cheerio');
 
 
 
-  
+
 
